da52a01faa8c7158c81a4e631875a7ac4fb6e922
nifos <message@vlnifos.work>
2025-03-16 17:39:01 +0200

    feat: friends tab styles

--- electron-app/src/renderer/src/views/Friends.tsx
+++ electron-app/src/renderer/src/views/Friends.tsx
@@ -1,3 +1,10 @@
 export default function Friends(): JSX.Element {
-  return <div className="text-2xl font-bold">Friends</div>
+  return (
+    <div className="w-full h-full mt-24 flex flex-col flex-center items-center text-center">
+      <div className="bg-gray-600/50 backdrop-blur-sm rounded-none p-8">
+        <h1 className="text-8xl font-bold text-left text-gray-200">Friends</h1>
+        <div className="text-2xl font-bold text-gray-300">To Be Implemented</div>
+      </div>
+    </div>
+  )
 }
